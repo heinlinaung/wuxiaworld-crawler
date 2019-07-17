149e8b0de40d694c26deb42c34db75034630a22d
Hein Lin Aung <heinlinaung6891@gmail.com>
2019-07-17 11:53:38 +0630

    Updated index.js(added Configs[hard-coded], loop and generated expected urls, Configs will be from User Input later)

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Crawler = require('js-crawler');
-const prefix_url = "https://www.wuxiaworld.com";
 
 var config = {
+prefix_url : "https://www.wuxiaworld.com",
+type : "novel",
+novelName : "renegade-immortal",
+chapter_prefix : "rge-chapter-",
 startPage: 1200,
 endPage: 1250
 }
@@ -9,5 +12,7 @@ endPage: 1250
 var crawler = new Crawler().configure({ignoreRelative: false, depth: 2});
 for (var i = config.startPage; i <= config.endPage; i++) 
 {
-	console.log('i value is '+i);
+	// Expected url -> https://www.wuxiaworld.com/novel/renegade-immortal/rge-chapter-1165 
+	let url = config.prefix_url + '/' + config.type + '/' + config.novelName + '/' + config.chapter_prefix + i; 
+	console.log('URL is '+url);
 }
