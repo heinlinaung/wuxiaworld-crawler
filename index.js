ab02ccdfaf756bab7f605617b781a8cf03da04a3
Hein Lin Aung <heinlinaung6891@gmail.com>
2019-07-17 14:54:24 +0630

    Added 'cherrio' package

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ type : "novel",
 novelName : "renegade-immortal",
 chapter_prefix : "rge-chapter-",
 startPage: 1200,
-endPage: 1250
+endPage: 1200
 }
 
-var crawler = new Crawler().configure({ignoreRelative: false, depth: 2});
+var crawler = new Crawler().configure({ignoreRelative: false, depth: 1});
 for (var i = config.startPage; i <= config.endPage; i++) 
 {
 	// Expected url -> https://www.wuxiaworld.com/novel/renegade-immortal/rge-chapter-1165 
 	let url = config.prefix_url + '/' + config.type + '/' + config.novelName + '/' + config.chapter_prefix + i; 
 	console.log('URL is '+url);
+	crawler.crawl({
+  		url:url, 
+		success: function(page) {
+    			console.log(page.content);
+ 		 },
+  		failure: function(page) {
+    			console.log(page.status);
+  		}
+	});
 }
